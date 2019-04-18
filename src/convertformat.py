06426fde44deca678ed724aa13f00099f8d02afd
Otazoman <ujimasa@hotmail.com>
2019-04-18 13:54:59 +0900

    modified convertformat.py divide getfeedword

--- src/convertformat.py
+++ src/convertformat.py
@@ -12,6 +12,26 @@ import traceback
 
 import feedparser
 
+def getfeedword(input_file):
+    """
+    RSSフィードを保存したファイルからタイトルと説明文を取得する。
+    """
+    try:
+        o = []
+        with open(input_file, newline = "") as f:
+             df = json.load(f)
+             for i in range(len(df)):
+                 for j in range(len(df[i])):
+                     if df[i] is not None or type(df[i]) is not 'NoneType':
+                        t = df[i][j]['title']
+                        s = re.sub('<.*?>', "", df[i][j]['description'])
+                        o +=[t,s]
+             return o
+    except Exception as e:
+        t, v, tb = sys.exc_info()
+        print(traceback.format_exception(t,v,tb))
+        print(traceback.format_tb(e.__traceback__))
+
 def main():
     """
     主処理
@@ -20,21 +40,9 @@ def main():
     try:
         start_t = time.perf_counter()
         input_file = sys.argv[1]
-
-        t = []
-        d = []
-
-        with open(input_file, newline = "") as f:
-             df = json.load(f)
-             for i in range(len(df)):
-                 for j in range(len(df[i])):
-                     if df[i] is not None or type(df[i]) is not 'NoneType':
-                        t.append(df[i][j]['title'])
-                        s = re.sub('<.*?>', "", df[i][j]['description'])
-                        #s = df[i][j]['description']
-                        d.append(s)
-             print(t)
-             print(d)
+        
+        r = getfeedword(input_file)
+        print(r)
 
         end_t = time.perf_counter()
         process_time = end_t - start_t
