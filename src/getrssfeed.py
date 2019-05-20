356ffc4acfcb431fca92f8c59c81d205fe53984a
Otazoman <ujimasa@hotmail.com>
2019-05-20 10:33:30 +0900

    20190520 Thread renewal

--- src/getrssfeed.py
+++ src/getrssfeed.py
@@ -3,9 +3,10 @@
 import csv
 import collections
 import json
-from multiprocessing import Pool
+#from multiprocessing import Pool
 import pprint
 import sys
+from concurrent.futures import ThreadPoolExecutor
 import time
 import traceback
 
@@ -41,7 +42,7 @@ def rssparse(feedurl):
                if hasattr(x, 'title') and \
                   hasattr(x, 'description') and \
                   hasattr(x, 'href') and \
-                  hasattr(x, 'updated_parsed'):            
+                  hasattr(x, 'updated_parsed'):
                   values = [x.title,x.description,x.links[0].href,time.strftime('%Y-%m-%d %H:%M:%S', x.updated_parsed)]
                   o = dict(zip(keys,values))
                   result.append(o)
@@ -63,8 +64,9 @@ def main():
         urls = getfeedurl(input_file)
         size = len(urls)
         print('RSSフィード数:{0}件'.format(size))
-        p = Pool(10)
-        out = p.map(rssparse, urls)
+        wk=10
+        with ThreadPoolExecutor(max_workers=wk) as executor:
+             out = list(executor.map(rssparse, urls))
         with open(output_file,'w') as f:
              f.write(json.dumps(out, indent=2, ensure_ascii=False))
         end_t = time.perf_counter()
@@ -74,8 +76,6 @@ def main():
         t, v, tb = sys.exc_info()
         print(traceback.format_exception(t,v,tb))
         print(traceback.format_tb(e.__traceback__))
-    finally:
-        p.close()
 
 if __name__ == '__main__':
    main()
