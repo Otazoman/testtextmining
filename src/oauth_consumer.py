0fb9e3f2fe20e93a22350d63aa1caa669dbd8888
Otazoman <ujimasa@hotmail.com>
2019-05-30 17:27:52 +0900

    20190530 Hateb

---
+++ src/oauth_consumer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+from flask import Flask, request, redirect, session
+from furl import furl
+from requests_oauthlib import OAuth1Session
+
+app = Flask(__name__)
+# xxxxxには、取得したランダムなシークレットキーを入力する。
+app.secret_key = xxxxx
+
+OAUTH_CONSUMER_KEY = yyyyy
+OAUTH_CONSUMER_SECRET = zzzzz
+
+TEMPORARY_CREDENTIAL_REQUEST_URL = 'https://www.hatena.com/oauth/initiate'
+RESOURCE_OWNER_AUTHORIZATION_URL = 'https://www.hatena.ne.jp/oauth/authorize'
+TOKEN_REQUEST_URL = 'https://www.hatena.com/oauth/token'
+
+CALLBACK_URI = 'http://127.0.0.1:5000/callback_page'
+SCOPE = {'scope': 'read_public,write_public'}
+
+@app.route('/')
+def index():
+    oauth = OAuth1Session(OAUTH_CONSUMER_KEY, client_secret=OAUTH_CONSUMER_SECRET, callback_uri=CALLBACK_URI)
+    fetch_response = oauth.fetch_request_token(TEMPORARY_CREDENTIAL_REQUEST_URL, data=SCOPE)
+
+    session['request_token'] = fetch_response.get('oauth_token')
+    session['request_token_secret'] = fetch_response.get('oauth_token_secret')
+
+    redirect_url = furl(RESOURCE_OWNER_AUTHORIZATION_URL)
+    redirect_url.args['oauth_token'] = session['request_token']
+    return redirect(redirect_url.url)
+
+@app.route('/callback_page')
+def callback_page():
+    verifier = request.args.get('oauth_verifier')
+    oauth = OAuth1Session(OAUTH_CONSUMER_KEY,
+                          client_secret=OAUTH_CONSUMER_SECRET,
+                          resource_owner_key=session['request_token'],
+                          resource_owner_secret=session['request_token_secret'],
+                          verifier=verifier)
+
+    access_tokens = oauth.fetch_access_token(TOKEN_REQUEST_URL)
+    access_token = access_tokens.get('oatuh_token')
+    access_secret = access_tokens.get('oauth_token_secret')
+    return "access_token: {}, access_token_secret: {}".format(access_token, access_secret)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- src/snspost.py
+++ src/snspost.py
@@ -1,5 +1,7 @@
 import json
+import requests
 from requests_oauthlib import OAuth1Session
+from requests_oauthlib import OAuth1
 import pprint
 import sys
 import traceback
@@ -7,17 +9,17 @@ import yaml
 
 
 
-def gettwitter_param():
+def get_authkey(snsname):
     """
     ymlファイルから設定情報を取得する。
     """
     try:    
         with open('../config/config.yml', 'r') as yml:
              config = yaml.load(yml)
-             consumer_key = config['twitter']['consumer_key']
-             consumer_secret = config['twitter']['consumer_secret']
-             token = config['twitter']['token']
-             token_secret = config['twitter']['token_secret']
+             consumer_key = config[snsname]['consumer_key']
+             consumer_secret = config[snsname]['consumer_secret']
+             token = config[snsname]['token']
+             token_secret = config[snsname]['token_secret']
 
              result = dict(ck=consumer_key,cs=consumer_secret, 
                            at=token,ats=token_secret) 
@@ -27,18 +29,17 @@ def gettwitter_param():
         print(traceback.format_exception(t,v,tb))
         print(traceback.format_tb(e.__traceback__))
 
-def main():
+def post_twitter(postword):
     """
-    主処理　twitterにつぶやいて結果表示 
+    twitterに投稿
 
     """
     try:
-        tp = gettwitter_param()
-        twitter = OAuth1Session(tp['ck'],tp['cs'],tp['at'],tp['ats'])
+        ta = get_authkey('twitter')
+        twitter = OAuth1Session(ta['ck'],ta['cs'],ta['at'],ta['ats'])
         get_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
         post_url = "https://api.twitter.com/1.1/statuses/update.json"
-        print("呟く内容は？")
-        tweet = input('>> ')
+        tweet = postword
         print('-------------------------------')
         params = {"status" : tweet}
         req = twitter.post(post_url, params = params)
@@ -53,6 +54,51 @@ def main():
                   print('----------------------------------------------------')
         else:
            print("ERROR : %d" % req.status_code)
+    except Exception as e:
+        t, v, tb = sys.exc_info()
+        print(traceback.format_exception(t,v,tb))
+        print(traceback.format_tb(e.__traceback__))
+
+
+def post_hatena(postword):
+    """
+    はてなブックマークに追加
+    """
+    try:
+        ta = get_authkey('hatena')
+        pprint.pprint(ta) 
+
+        auth = OAuth1(ta['ck'],ta['cs'],ta['at'],ta['ats'])
+
+        bookmark_api_url = "http://api.b.hatena.ne.jp/1/my/bookmark"
+        bookmark_url = "https://google.com"
+        
+        req = requests.post(bookmark_api_url + "?url=" + bookmark_url, auth=auth)
+        
+        if req.status_code == 200:
+           print('SUCCESS')
+        else:
+           print("ERROR : %d" % req.status_code)
+
+
+    except Exception as e:
+        t, v, tb = sys.exc_info()
+        print(traceback.format_exception(t,v,tb))
+        print(traceback.format_tb(e.__traceback__))
+
+
+def main():
+    """
+    主処理　twitterにつぶやいて結果表示 
+
+    """
+    try:
+        print("呟く内容は？")
+        content = input('>> ')
+        #post_twitter(content)
+        post_hatena(content)
+    
+    
     except Exception as e:
         t, v, tb = sys.exc_info()
         print(traceback.format_exception(t,v,tb))
